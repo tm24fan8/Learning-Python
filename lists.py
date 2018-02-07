9757ad6fab865522ac53037e2ea41b3c97f1e27d
tm24fan8 <tm24fan8@gmail.com>
2018-02-07 05:09:53 -0500

    Lists
    Added lists examples

---
+++ lists.py
@@ -0,0 +1,11 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])  # Prints 1
+print(mylist[1])  # Prints 2
+print(mylist[2])  # Prints 3
+
+# prints out 1,2,3
+for x in mylist:
+    print(x)
