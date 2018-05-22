3e95b20ddcb19bad3a8080ad2a0fe27a4ddf04d1
tm24fan8 <tm24fan8@gmail.com>
2018-05-22 00:00:08 -0400

    Lesson 1

---
+++ new/basic-io.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = input('Enter your name: ')
+print("Hello", name)
+
+age = input('How old are you: ')
+print("Next year, you will be", int(age) + 1)
---
+++ new/variables.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+pi = 3.14
+r = 10
+
+print((int(pi) * int(r))**2)
