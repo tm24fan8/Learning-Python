ef4ce4ff2fbca8b63cf0c5b8d7855b5f6d59655d
tm24fan8 <tm24fan8@gmail.com>
2018-05-23 17:27:30 -0400

    Numbers and math

---
+++ new/numbers/calculations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Calculate average sleep for the week
+
+sleep = 6.2 + 7 + 8 + 5 + 6.5 + 7.1 + 8.5
+avgsleep = sleep / 7
+print(avgsleep)
+
+# Is 297 divisible by 3
+
+print(int(297 % 3))
+
+# 2 to the tenth power
+
+print(int(2**10))
+
+# Determine leap years
+
+print(int(1800 % 400), int(1900 % 400), int(1903 % 4), int(2000 % 400), int(2002 % 4))
