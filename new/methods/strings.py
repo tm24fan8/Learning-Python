a5745d930ce2e8a096dfac02a8fa8f891b59e528
tm24fan8 <tm24fan8@gmail.com>
2018-05-23 19:39:10 -0400

    Methods

---
+++ new/methods/strings.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Calling capitalize method, on variable and directly
+
+name = 'matt'
+
+correct = name.capitalize()
+print(correct)
+print(name)
+print('fred'.capitalize())
+
+# Join
+
+print(', '.join(['1', '2', '3']))
+
+# Lower
+
+fname = 'readme.txt'
+print(fname.endswith('txt') or fname.endswith('TXT'))
+print(fname.lower().endswith('txt'))
